fa9a3017775c710a32551834d20345e5235e8bb3
yangkaile <guyexing@foxmail.com>
2019-05-30 09:24:46 +0800

    添加日期工具类

--- common/src/main/java/com/kyle/mycommon/util/DateUtils.java
+++ common/src/main/java/com/kyle/mycommon/util/DateUtils.java
@@ -2,15 +2,17 @@ package com.kyle.mycommon.util;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.Logger;
 
 /**
  * @author yangkaile
  * @date 2019-05-30 08:55:13
  */
 public class DateUtils {
-    private static Logger logger = Logger.getLogger(DateUtils.class.getSimpleName());
-
+    /**
+     * 获取时间字符串
+     * @param date
+     * @return
+     */
     public static String getDate(Date date){
         SimpleDateFormat simpleDateFormat =
                 new SimpleDateFormat("yyyy-MM-dd");
@@ -20,6 +22,11 @@ public class DateUtils {
         return getDate(new Date());
     }
 
+    /**
+     * 获取日期时间字符串
+     * @param date
+     * @return
+     */
     public static String getDateTime(Date date){
         SimpleDateFormat simpleDateFormat =
                 new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -30,6 +37,11 @@ public class DateUtils {
         return getDateTime(new Date());
     }
 
+    /**
+     * 获取时间标识，年月日时分秒毫秒，用于文件名
+     * @param date
+     * @return
+     */
     public static String getTimeMask(Date date){
         SimpleDateFormat simpleDateFormat =
                 new SimpleDateFormat("yyyyMMddHHmmssSSS");
@@ -41,8 +53,8 @@ public class DateUtils {
     }
 
     public static void main(String[] args){
-        logger.info(DateUtils.getDate());
-        logger.info(DateUtils.getDateTime());
-        logger.info(DateUtils.getTimeMask());
+        ConsoleLogUtils.print("getDate",DateUtils.getDate());
+        ConsoleLogUtils.print("getDateTime",DateUtils.getDateTime());
+        ConsoleLogUtils.print("getTimeMask",DateUtils.getTimeMask());
     }
 }
