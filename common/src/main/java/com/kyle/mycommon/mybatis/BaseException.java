951500b2bcd7361670599354dc177ec7adc2b541
yangkaile <guyexing@foxmail.com>
2019-07-18 09:55:05 +0800

    添加注释

--- common/src/main/java/com/kyle/mycommon/mybatis/BaseException.java
+++ common/src/main/java/com/kyle/mycommon/mybatis/BaseException.java
@@ -1,7 +1,9 @@
 package com.kyle.mycommon.mybatis;
 
 /**
- * Created by yangkaile on 2019/7/17.
+ * @author yangkaile
+ * @date 2019-07-18 09:49:17
+ * 自定义异常，用来处理BaseMapper使用过程中抛出的异常
  */
 public class BaseException extends Exception{
     public BaseException(String message){
